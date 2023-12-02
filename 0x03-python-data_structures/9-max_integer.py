fdab2d0f154c1cbea49fbdab6023241098859bd2
nasirolamirewaju <nasirolamirewaju10@gmail.com>
2023-12-01 23:20:54 -0800

    MaxInteger

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+
 def max_integer(my_list=[]):
-    if not my_list:
-        return None
+    if len(my_list) == 0:
+        return (None)
     else:
-        my_list.sort()
-        return((my_list[-1]))
+        max_int = my_list[0]
+        for i in range(len(my_list)):
+            if my_list[i] > max_int:
+                max_int = my_list[i]
+        return (max_int)
